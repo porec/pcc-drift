c3ac37102bc317699e966bfdb3b33c747a284d01
dreshytnik <32958664+dreshytnik@users.noreply.github.com>
2022-07-18 09:10:37 +0200

    initial

---
+++ ec2-ubuntu.tf
@@ -0,0 +1,45 @@
+# Simple instance template
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_security_group" "dima-SG" {
+  name = "dima-ssh_traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "dima-EC2" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.dima-SG.name}"]
+  key_name        = "linux-key-pair"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
